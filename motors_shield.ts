62e927cdb2e68baf67daaefc626f7f554ca6cf99
LouisBERNOT <Louis.bernot@hotmail.Fr>
2019-05-09 15:07:21 +0200

    Update motors_shield.ts

--- motors_shield.ts
+++ motors_shield.ts
@@ -1,7 +1,7 @@
 /**
  * Librairie A4 technologie pour robot <nom du robot>.
  */
-//% weight=999 color=#004696 icon="\uf121" block="A4 Technologie v2" advanced=false
+//% weight=100 color=#004696 icon="\uf121" block="A4 Technologie v2" advanced=false
 namespace A4_Robot_Driver {
 	/************************************************************************************************************************************************
 	* micro:bit motor driver blocks 
